e0e4a2ba0b133729292b87e106dcda125b31c843
Pestmite <Charleshenrichaki@gmail.com>
2025-01-22 08:02:52 -0500

    Update main.js

--- Robotics Website/javascript/main.js
+++ Robotics Website/javascript/main.js
@@ -1,5 +1,27 @@
-// Code adapted from Joseph Silber on Stack Overflow (lines 4-12): 
-// https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
+/* || Header Scroll */
+
+let startY = 0;
+let HIDEMIN = 350;
+
+window.addEventListener('scroll', () => {
+  const currentY = window.scrollY;
+
+  if (currentY > HIDEMIN) {
+    if (currentY > startY && innerWidth > 660) {
+      document.querySelector('.header').style.transform = 'translateY(-150%)';
+    } else {
+      document.querySelector('.header').style.transform = 'translateY(0)';
+    }
+  } else {
+    document.querySelector('.header').style.transform = 'translateY(0)';
+  }
+
+  startY = currentY;
+});
+
+/* || Subheader Scroll */
+
+console.log(document.querySelector('.header'));
 
 document.querySelectorAll('a[href^="#"]').forEach(navLink => {
   navLink.addEventListener('click', function (event) {
@@ -11,6 +33,8 @@ document.querySelectorAll('a[href^="#"]').forEach(navLink => {
   });
 });
 
+/* || Arrow Animation */
+
 document.querySelector('.arrow-container').addEventListener('mouseover', () => {
   document.querySelector('.footer-arrow').classList.add('arrow-js');
 });
@@ -19,10 +43,9 @@ document.querySelector('.arrow-container').addEventListener('mouseout', () => {
   document.querySelector('.footer-arrow').classList.remove('arrow-js');
 });
 
-document.querySelector('.translate-element').addEventListener('mouseover', () => {
-  document.querySelector('.translate').classList.add('translate-js');
-});
+/* || Mobile menu */
 
-document.querySelector('.translate-element').addEventListener('mouseout', () => {
-  document.querySelector('.translate').classList.remove('translate-js');
+document.querySelector('.menu').addEventListener('click', () => {
+  document.querySelector('.menu').classList.toggle('active');
+  document.querySelector('.off-screen-menu').classList.toggle('active');
 });
